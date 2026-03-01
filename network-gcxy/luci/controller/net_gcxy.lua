d91e5f1a305c3b67bd435d6c8dd8cb8203e71ce5
SagitPixel <59535808+SagitPixel@users.noreply.github.com>
2026-03-01 13:23:11 +0800

    feat: 整合原版监控逻辑，修复IP获取，优化Makefile权限管理

--- network-gcxy/luci/controller/net_gcxy.lua
+++ network-gcxy/luci/controller/net_gcxy.lua
@@ -1,6 +1,5 @@
 module("luci.controller.net_gcxy", package.seeall)
 
 function index()
-    -- entry(路径, 对应的CBI模型, 菜单标题, 排序权重)
-    entry({"admin", "network", "gcxy"}, cbi("net_gcxy"), _("Campus Network"), 10).dependent = true
+    entry({"admin", "network", "gcxy"}, cbi("net_gcxy"), _("校园网认证"), 10).dependent = true
 end
